05c9d31b87fb233270d2aa91dc7332b42c25b27f
Arminas Bražėnas <80688723+arminasbrazenas@users.noreply.github.com>
2023-09-17 20:34:20 +0300

    Implement validateDataFrame function (#1)

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -8,7 +8,7 @@ module Lib1
   )
 where
 
-import DataFrame (DataFrame)
+import DataFrame (DataFrame (..), Row, Column (..), ColumnType (..), Value (..))
 import InMemoryTables (TableName)
 
 type ErrorMessage = String
@@ -30,7 +30,38 @@ parseSelectAllStatement _ = error "parseSelectAllStatement not implemented"
 -- 3) implement the function which validates tables: checks if
 -- columns match value types, if rows sizes match columns,..
 validateDataFrame :: DataFrame -> Either ErrorMessage ()
-validateDataFrame _ = error "validateDataFrame ot implemented"
+validateDataFrame (DataFrame cols rows) = validateRows rows
+    where
+        validateRows :: [Row] -> Either ErrorMessage ()
+        validateRows [] = Right ()
+        validateRows (r:rs) = 
+            case validateRow cols r of
+                Left err -> Left err
+                Right _ -> validateRows rs
+
+        validateRow :: [Column] -> [Value] -> Either ErrorMessage ()
+        validateRow [] [] = Right ()
+        validateRow (_:_) [] = Left "Row does not have enough values."
+        validateRow [] (_:_) = Left "Row has too many values."
+        validateRow (c:cs) (v:vs)
+            | doesValueTypeMatchColType c v = validateRow cs vs
+            | otherwise = Left "Cell's value type does not match the column's type."
+
+        doesValueTypeMatchColType :: Column -> Value -> Bool
+        doesValueTypeMatchColType _ NullValue = True
+        doesValueTypeMatchColType col val =
+            case getColTypeByValue val of
+                Left _ -> False
+                Right colType -> colType == getColType col
+
+        getColTypeByValue :: Value -> Either ErrorMessage ColumnType
+        getColTypeByValue (IntegerValue _) = Right IntegerType
+        getColTypeByValue (StringValue _) = Right StringType
+        getColTypeByValue (BoolValue _) = Right BoolType
+        getColTypeByValue _ = Left "Unknown column type."
+
+        getColType :: Column -> ColumnType
+        getColType (Column _ t) = t
 
 -- 4) implement the function which renders a given data frame
 -- as ascii-art table (use your imagination, there is no "correct"

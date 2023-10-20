90d617b1abb546aa042ee0b99557612d31721890
GitaGliaudyte <gita.gliaudyte@gmail.com>
2023-10-21 00:28:10 +0300

    YIPPEE

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -14,15 +14,16 @@ import DataFrame
       Column(..),
       ColumnType(..),
       Value(..),
-      DataFrame(DataFrame) )
+      DataFrame)
 import InMemoryTables (TableName, database)
-import Data.List.NonEmpty (some1)
+import Data.List.NonEmpty (some1, xor)
 import Foreign.C (charIsRepresentable)
 import Data.Char (toLower, GeneralCategory (ParagraphSeparator))
 import qualified InMemoryTables as DataFrame
 import Lib1 (renderDataFrameAsTable, findTableByName)
 import Data.List (isPrefixOf)
 import Data.Maybe (fromMaybe)
+import Text.ParserCombinators.ReadP (get)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
@@ -58,11 +59,31 @@ instance Monad Parser where
 instance MonadFail Parser where
   fail _ = Parser $ \_ -> Left "Monad failed"
 
+class (Applicative f) => Alternative f where
+  empty :: f a
+  ( <|> ) :: f a -> f a -> f a
+  some :: f a -> f [a]
+  some v = some_v
+    where many_v = some_v <|> pure []
+          some_v = (:) <$> v <*> many_v
+
+  many :: f a -> f [a]
+  many v = many_v
+    where many_v = some_v <|> pure []
+          some_v = (:) <$> v <*> many_v
+
+instance Alternative Parser where
+  empty = fail ""
+  (Parser x) <|> (Parser y) = Parser $ \s ->
+    case x s of
+      Right x -> Right x
+      Left _ -> y s
+
 char :: Char -> Parser Char
 char c = Parser charP
-  where charP []                 = Left "Empty"
+  where charP []                 = Left "No table name was provided"
         charP (x:xs) | x == c    = Right (c, xs)
-                     | otherwise = Left "Do not match"
+                     | otherwise = Left "Table with this name does not exist or ; is missing"
 
 string :: String -> Parser String
 string = mapM char
@@ -86,14 +107,14 @@ findTuples db = map firstFromTuple db
 firstFromTuple :: (TableName, DataFrame) -> TableName
 firstFromTuple = fst
 
-parserToStatement :: Parser String -> ParsedStatement
-parserToStatement p = case runParser p "" of
+parserToStatement :: Parser String -> String -> ParsedStatement
+parserToStatement p s = case runParser p s of
   Right (parsedStr, _) -> ShowTable parsedStr -- Here, you should pattern match the parsed string to create a ParsedStatement
   Left err -> Err err -- Handle the case where parsing fails
 
 parseTableName :: String -> Parser String
 parseTableName str = do
-  case lookup str database of
+  case lookup str InMemoryTables.database of
     Just _ -> string str
     Nothing -> string "Table not found in the database"
 
@@ -102,18 +123,28 @@ parseTableName str = do
 parseStatement :: String -> Either ErrorMessage ParsedStatement
 parseStatement query
   | toLowerString query == "show tables;" = Right (ShowTables findTableNames)--(ShowTable )--(parserToStatement (string query))--Right (string query)
-  | "show table" `isPrefixOf` toLowerString query = Right (parserToStatement (parseTableName (drop 11 (init query))))
+  | "show table" `isPrefixOf` toLowerString query = Right (parserToStatement (parseTableName (drop 11 (init query))) (drop 11 (init query)))
   | otherwise = Left "FAILED."
 
 -- Executes a parsed statement. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
 
 executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
-executeStatement (ShowTable tableName) = Right (fromMaybe (createTablesDataFrame []) (lookup (show tableName) database))
+executeStatement (ShowTable tableName) = Right (createColumnsDataFrame (columnsToList (fromMaybe (DataFrame [] []) (lookup tableName InMemoryTables.database))) tableName)
 executeStatement (ShowTables tableNames) = Right (createTablesDataFrame tableNames)
 executeStatement (Err err) = Left err
---executeStatement _  = Left "Not implemented: executeStatement"
---executeStatement ps = 
+
 
 createTablesDataFrame :: [TableName] -> DataFrame
 createTablesDataFrame tableNames = DataFrame [Column "Tables" StringType] (map (\name -> [StringValue name]) tableNames)
+
+createColumnsDataFrame :: [String] -> String -> DataFrame
+createColumnsDataFrame columnNames columnTableName = DataFrame [Column columnTableName StringType] (map (\name ->  [StringValue name]) columnNames)
+
+columnsToList :: DataFrame -> [String]
+columnsToList (DataFrame [] []) = []
+columnsToList (DataFrame columns _) = map getColumnName columns
+
+getColumnName :: Column -> String
+getColumnName (Column "" _) = ""
+getColumnName (Column columnname _) = columnname

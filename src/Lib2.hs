3d3467c43cf7c6d390dfc8d33b14ce5a4f0f9c07
GitaGliaudyte <gita.gliaudyte@gmail.com>
2023-10-20 21:51:15 +0300

    Zinute

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -20,19 +20,20 @@ import Data.List.NonEmpty (some1)
 import Foreign.C (charIsRepresentable)
 import Data.Char (toLower, GeneralCategory (ParagraphSeparator))
 import qualified InMemoryTables as DataFrame
-import Lib1 (renderDataFrameAsTable)
+import Lib1 (renderDataFrameAsTable, findTableByName)
 import Data.List (isPrefixOf)
+import Data.Maybe (fromMaybe)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
 
 -- Keep the type, modify constructors
 data ParsedStatement = ParsedStatement
-  | ShowTable TableName--TableName
+  | ShowTable String
   | ShowTables [TableName]
-  | Kazkas String
+  | Err ErrorMessage
   deriving (Show)
-
+--------------------------------------------------------------------------------
 newtype Parser a = Parser {
     runParser :: String -> Either ErrorMessage (a, String)
 }
@@ -61,10 +62,12 @@ char :: Char -> Parser Char
 char c = Parser charP
   where charP []                 = Left "Empty"
         charP (x:xs) | x == c    = Right (c, xs)
-                     | otherwise = Left "Nebelika"
+                     | otherwise = Left "Do not match"
 
 string :: String -> Parser String
 string = mapM char
+
+----------------------------------------------------------------------------------
 
 charToString :: Char -> String
 charToString c = [c]
@@ -72,7 +75,6 @@ charToString c = [c]
 toLowerString :: String -> String
 toLowerString [] = ""
 toLowerString (x:xs) = charToString (toLower x) ++ toLowerString xs
-
 
 findTableNames :: [TableName]
 findTableNames = findTuples InMemoryTables.database
@@ -84,27 +86,34 @@ findTuples db = map firstFromTuple db
 firstFromTuple :: (TableName, DataFrame) -> TableName
 firstFromTuple = fst
 
---parserToStatement :: Parser String -> ParsedStatement
---parserToStatement p = case runParser p "" of
---  Right (parsedStr, _) -> ShowTable parsedStr -- Here, you should pattern match the parsed string to create a ParsedStatement
---  Left err -> ParsedStatement -- Handle the case where parsing fails
+parserToStatement :: Parser String -> ParsedStatement
+parserToStatement p = case runParser p "" of
+  Right (parsedStr, _) -> ShowTable parsedStr -- Here, you should pattern match the parsed string to create a ParsedStatement
+  Left err -> Err err -- Handle the case where parsing fails
+
+parseTableName :: String -> Parser String
+parseTableName str = do
+  case lookup str database of
+    Just _ -> string str
+    Nothing -> string "Table not found in the database"
 
 -- Parses user input into an entity representing a parsed
 -- statement
 parseStatement :: String -> Either ErrorMessage ParsedStatement
 parseStatement query
   | toLowerString query == "show tables;" = Right (ShowTables findTableNames)--(ShowTable )--(parserToStatement (string query))--Right (string query)
-  | "show table " `isPrefixOf` toLowerString query = Right (Kazkas (drop 11 (init query)))
-  | otherwise = Left "NEATEJOM"
+  | "show table" `isPrefixOf` toLowerString query = Right (parserToStatement (parseTableName (drop 11 (init query))))
+  | otherwise = Left "FAILED."
 
 -- Executes a parsed statement. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
 
 executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
+executeStatement (ShowTable tableName) = Right (fromMaybe (createTablesDataFrame []) (lookup (show tableName) database))
 executeStatement (ShowTables tableNames) = Right (createTablesDataFrame tableNames)
+executeStatement (Err err) = Left err
 --executeStatement _  = Left "Not implemented: executeStatement"
 --executeStatement ps = 
 
 createTablesDataFrame :: [TableName] -> DataFrame
-createTablesDataFrame tableNames =
-    DataFrame [Column "Tables" StringType] (map (\name -> [StringValue name]) tableNames)
+createTablesDataFrame tableNames = DataFrame [Column "Tables" StringType] (map (\name -> [StringValue name]) tableNames)

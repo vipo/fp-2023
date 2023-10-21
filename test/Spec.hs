7f5aa09a7e2ab05b6fefdfa740b53525533e0e27
Emilis <emiliskleinas2003@gmail.com>
2023-10-21 17:49:12 +0300

    bug fix 4

--- test/Spec.hs
+++ test/Spec.hs
@@ -75,8 +75,8 @@ main = hspec $ do
 
   describe "filterRowsByBoolColumn" $ do
     it "should return list of matching rows" $ do
-      filterRowsByBoolColumn (snd D.tableWithNulls) (Column "value" BoolType) True `shouldBe` Right DataFrame [Column "flag" StringType, Column "value" BoolType] [[StringValue "a", BoolValue True], [StringValue "b", BoolValue True]]
-      filterRowsByBoolColumn (snd D.tableWithNulls) (Column "value" BoolType) False `shouldBe` Right DataFrame [Column "flag" StringType, Column "value" BoolType] [[StringValue "b", BoolValue False]]
+      filterRowsByBoolColumn (snd D.tableWithNulls) (Column "value" BoolType) True `shouldBe` Right (DataFrame [Column "flag" StringType, Column "value" BoolType] [[StringValue "a", BoolValue True], [StringValue "b", BoolValue True]])
+      filterRowsByBoolColumn (snd D.tableWithNulls) (Column "value" BoolType) False `shouldBe` Right (DataFrame [Column "flag" StringType, Column "value" BoolType] [[StringValue "b", BoolValue False]])
 
     it "should return Error if Column is not bool type" $ do
       filterRowsByBoolColumn (snd D.tableWithNulls) (Column "flag" StringType) True `shouldBe` Left "Dataframe does not contain column by specified name or column is not of type bool"

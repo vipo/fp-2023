8b9b75aab471204dc2dae7d1b0c4897894727672
Emilis <emiliskleinas2003@gmail.com>
2023-10-21 15:12:48 +0300

    bug fix v2

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,21 +1,21 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module Lib2
   ( parseStatement,
     executeStatement,
-    ParsedStatement(..)
+    filterRowsByBoolColumn,
+    ParsedStatement (..),
   )
 where
 
 import Data.Char (toLower)
-import DataFrame (DataFrame(..), Column(..), ColumnType(..), Value(..), Row)
-import InMemoryTables (TableName, database)
-import Control.Monad (when)
-import Text.Read (Lexeme(Number))
 import Data.List (elemIndex)
+import DataFrame (Column (..), ColumnType (..), DataFrame (..), Row, Value (..))
+import InMemoryTables (TableName, database)
 
 type ErrorMessage = String
+
 type Database = [(TableName, DataFrame)]
 
 data ParsedStatement
@@ -30,10 +30,10 @@ parseStatement input
   | last input /= ';' = Left "Unsupported or invalid statement"
   | otherwise =
       let cleanedInput = init input
-      in case words (map toLower cleanedInput) of
-          ["show", "table", tableName] -> Right $ ShowTable tableName
-          ["show", "tables"] -> Right ShowTables
-          _ -> Left "Unsupported or invalid statement"
+       in case words (map toLower cleanedInput) of
+            ["show", "table", tableName] -> Right $ ShowTable tableName
+            ["show", "tables"] -> Right ShowTables
+            _ -> Left "Unsupported or invalid statement"
 
 -- Executes a parsed statemet. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
@@ -43,21 +43,22 @@ executeStatement (ShowTable tableName) =
     Just df -> Right $ DataFrame [Column "Columns" StringType] (map (\(Column name _) -> [StringValue name]) (columns df))
     Nothing -> Left $ "Table " ++ tableName ++ " not found"
 executeStatement ShowTables =
-  Right $ DataFrame [Column "Tables" StringType]
-                   (map (\(name, _) -> [StringValue name]) database)  -- Produce a DataFrame listing all table names
+  Right $
+    DataFrame
+      [Column "Tables" StringType]
+      (map (\(name, _) -> [StringValue name]) database) -- Produce a DataFrame listing all table names
 
 -- Extract the columns of a DataFrame
 columns :: DataFrame -> [Column]
 columns (DataFrame cols _) = cols
-
 
 -- Filter rows based on whether the specified column's value is TRUE or FALSE.
 
 filterRowsByBoolColumn :: (TableName, DataFrame) -> Column -> Bool -> Either ErrorMessage [Row]
 filterRowsByBoolColumn (_, DataFrame cols rows) col bool
   | col `elem` cols && getColumnType col == BoolType = Right $ getRowsByBool bool rows
-  | otherwise = Left "Dataframe does not contain column by specified name or column is not of type bool" where
-
+  | otherwise = Left "Dataframe does not contain column by specified name or column is not of type bool"
+  where
     getColumnType :: Column -> ColumnType
     getColumnType (Column _ columnType) = columnType
 

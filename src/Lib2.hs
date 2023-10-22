b52ee1a73ee7c342c09cb22f626426efea41c64c
Emilis <emiliskleinas2003@gmail.com>
2023-10-22 18:00:52 +0300

    Good parse

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -3,15 +3,14 @@
 module Lib2
   ( parseStatement,
     executeStatement,
-    ParsedStatement(..),
+    ParsedStatement (..),
   )
 where
 
 import Data.Char (toLower)
-import DataFrame (DataFrame (..), Column (..), ColumnType (..), Value (..), Row)
-import InMemoryTables (TableName, database)
 import Data.List (elemIndex, isPrefixOf, isSuffixOf)
-
+import DataFrame (Column (..), ColumnType (..), DataFrame (..), Row, Value (..))
+import InMemoryTables (TableName, database)
 
 type ErrorMessage = String
 
@@ -21,8 +20,8 @@ data ParsedStatement
   | AvgColumn TableName String
   deriving (Show, Eq)
 
---parseStatement :: String -> Either ErrorMessage ParsedStatement
---parseStatement input
+-- parseStatement :: String -> Either ErrorMessage ParsedStatement
+-- parseStatement input
 --  | last input /= ';' = Left "Unsupported or invalid statement"
 --  | otherwise =
 --    let cleanedInput = init input
@@ -32,7 +31,7 @@ data ParsedStatement
 --      ["show", "tables"] -> Right ShowTables
 --      "select" : rest ->
 --        case rest of
---          func : "from" : tableName : [] 
+--          func : "from" : tableName : []
 --            | "avg(" `isPrefixOf` func && ")" `isSuffixOf` func ->
 --                let columnName = drop 4 $ init func
 --                in Right $ AvgColumn tableName columnName
@@ -46,36 +45,44 @@ columnNameExists tableName columnName =
       any (\(Column name _) -> name == columnName) (columns df)
     Nothing -> False
 
-
 parseStatement :: String -> Either ErrorMessage ParsedStatement
 parseStatement input
   | last input /= ';' = Left "Unsupported or invalid statement"
   | otherwise =
-    let cleanedInput = init input
-        lowerInput = map toLower cleanedInput
-        wordsInput = words lowerInput
-    in case wordsInput of
-      ["show", "table", tableName] ->
-        if tableNameExists tableName
-          then Right $ ShowTable tableName
-          else Left "Table not found"
-      ["show", "tables"] -> Right ShowTables
-      "select" : rest ->
-        case rest of
-          func : "from" : tableName : [] 
-            | "avg(" `isPrefixOf` func && ")" `isSuffixOf` func ->
-                let columnName = drop 4 $ init func
-                in if tableNameExists tableName
-                   then Right $ AvgColumn tableName columnName
-                   else Left "Unsupported or invalid statement"
-          _ -> Left "Unsupported or invalid statement"
-      _ -> Left "Unsupported or invalid statement"
+      let cleanedInput = init input
+          wordsInput = parseSemiCaseSensitive cleanedInput
+       in case wordsInput of
+            ["show", "table", tableName] ->
+              if tableNameExists tableName
+                then Right $ ShowTable tableName
+                else Left "Table not found"
+            ["show", "tables"] -> Right ShowTables
+            "select" : rest ->
+              case rest of
+                func : "from" : tableName : []
+                  | "avg(" `isPrefixOf` func && ")" `isSuffixOf` func ->
+                      let columnName = drop 4 $ init func
+                       in if tableNameExists tableName
+                            then Right $ AvgColumn tableName columnName
+                            else Left "Unsupported or invalid statement"
+                _ -> Left "Unsupported or invalid statement"
+            _ -> Left "Unsupported or invalid statement"
 
+parseSemiCaseSensitive :: String -> [String]
+parseSemiCaseSensitive statement = convertedStatement
+  where
+    keywords = ["select", "from", "where"]
+    splitStatement = words statement
+    convertedStatement = map wordToLowerSensitive splitStatement
 
+    wordToLowerSensitive :: String -> String
+    wordToLowerSensitive word
+      | map toLower word `elem` keywords = map toLower word
+      | "avg(" `isPrefixOf` map toLower word && ")" `isSuffixOf` word = "avg(" ++ drop 4 (init word) ++ ")"
+      | otherwise = word
 
 tableNameExists :: TableName -> Bool
 tableNameExists name = any (\(tableName, _) -> tableName == name) database
-
 
 executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
 executeStatement (ShowTable tableName) =
@@ -91,14 +98,12 @@ executeStatement (AvgColumn tableName columnName) =
         Just columnIndex ->
           let values = map (\row -> getColumnValue columnIndex row) rows
               validIntValues = filter isIntegerValue values
-          in
-            if null validIntValues
-              then Left "No valid integers found in the specified column"
-              else
-                let sumValues = sumIntValues validIntValues
-                    avg = fromIntegral sumValues / fromIntegral (length validIntValues)
-                in
-                  Right $ DataFrame [Column "AVG" IntegerType] [[IntegerValue (round avg)]]
+           in if null validIntValues
+                then Left "No valid integers found in the specified column"
+                else
+                  let sumValues = sumIntValues validIntValues
+                      avg = fromIntegral sumValues / fromIntegral (length validIntValues)
+                   in Right $ DataFrame [Column "AVG" IntegerType] [[IntegerValue (round avg)]]
         Nothing -> Left $ "Column " ++ columnName ++ " not found in table " ++ tableName
     Nothing -> Left $ "Table " ++ tableName ++ " not found"
 
@@ -118,4 +123,3 @@ isIntegerValue _ = False
 
 sumIntValues :: [Value] -> Integer
 sumIntValues = foldr (\(IntegerValue x) acc -> x + acc) 0
-

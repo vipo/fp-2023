27c7e3ff17163d8453ae1103c183edbb8b45e914
Gintare Keraite <gintare.keraite18@gmail.com>
2023-11-18 18:18:37 +0200

    now(); functionality

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -3,22 +3,26 @@
 module Lib3
   ( executeSql,
     Execution,
-    ExecutionAlgebra(..)
+    ExecutionAlgebra(..),
+    loadFile,
+    getTime,
+    createDataFrame
   )
 where
 
 import Control.Monad.Free (Free (..), liftF)
-import DataFrame (DataFrame)
-import Data.Time ( UTCTime )
+import DataFrame (Column(..), ColumnType(..), Value(..), DataFrame(..))
+import Data.Time (UTCTime)
 
 type TableName = String
 type FileContent = String
 type ErrorMessage = String
+type SQLQuery = String
 
 data ExecutionAlgebra next
   = LoadFile TableName (FileContent -> next)
   | GetTime (UTCTime -> next)
-  -- feel free to add more constructors here
+  | ExecuteSQL SQLQuery (Either ErrorMessage DataFrame -> next)
   deriving Functor
 
 type Execution = Free ExecutionAlgebra
@@ -29,6 +33,16 @@ loadFile name = liftF $ LoadFile name id
 getTime :: Execution UTCTime
 getTime = liftF $ GetTime id
 
-executeSql :: String -> Execution (Either ErrorMessage DataFrame)
-executeSql sql = do
-    return $ Left "implement me"
+executeSql :: SQLQuery -> Execution (Either ErrorMessage DataFrame)
+executeSql "NOW();" = do
+    currentTime <- getTime
+    let df = createDataFrame currentTime
+    return $ Right df
+executeSql _ = return $ Left "wrong input"
+
+createDataFrame :: UTCTime -> DataFrame
+createDataFrame time =
+    let timeColumn = Column "Time" StringType
+        timeValue = StringValue $ show time
+        row = [timeValue]
+    in DataFrame [timeColumn] [row]

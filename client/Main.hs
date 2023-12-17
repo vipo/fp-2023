76965042c8958c5bf668d09fb4613723148b7ef1
Domarkaite <119532702+Domarkaite@users.noreply.github.com>
2023-12-17 18:53:05 +0200

    puuuuuuuuuuush

--- client/Main.hs
+++ client/Main.hs
@@ -1,17 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main (main) where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Free (Free (..))
 import Data.Functor((<&>))
 import Network.Wreq 
-import Control.Lens ((^.))
-import Network.HTTP.Client
+import Control.Lens ((^.), view)
 import Data.Time ( UTCTime, getCurrentTime )
 import Data.List qualified as L
 import Lib1 qualified
 import Lib2 qualified
 import Lib3 qualified
 import Lib4 qualified
+import DataFrame
+import InMemoryTables
+import Network.Wreq
+import Control.Lens
+import Data.Aeson (ToJSON, FromJSON)
+import Data.Yaml
+import Network.Wreq
+import GHC.Generics
+import Control.Lens
+import Data.Aeson (ToJSON, FromJSON)
+import Data.Yaml
+import Text.PrettyPrint
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy.Char8 as BSLC8
+import qualified Data.ByteString.Lazy as BSL
 import System.Console.Repline
   ( CompleterStyle (Word),
     ExitDecision (Exit),
@@ -24,6 +41,7 @@ import System.Console.Terminal.Size (Window, size, width)
 import Lib2 (tableNameParser)
 import System.Directory (doesFileExist, getDirectoryContents)
 import System.FilePath (pathSeparator)
+
 
 
 type Repl a = HaskelineT IO a
@@ -46,29 +64,48 @@ completer n = do
               ]
   return $ Prelude.filter (L.isPrefixOf n) names
 
+data ServerResponse = ServerResponse
+    { responseRight :: Maybe DataFrame
+    , responseLeft :: Maybe ErrorMessage
+    } deriving (Show, Eq, Generic)
+
+type ErrorMessage = String
+
+instance ToJSON ServerResponse
+instance FromJSON ServerResponse
+
+
 -- Evaluation : handle each line user inputs
 cmd :: String -> Repl ()
-cmd inp = do
+cmd input = do
   s <- terminalWidth <$> liftIO size
-  result <- liftIO $ cmd' s
+  result <- liftIO $ sendQuery input
   case result of
     Left err -> liftIO $ putStrLn $ "Error: " ++ err
-    Right table -> liftIO $ putStrLn table
+    Right df -> do
+        liftIO $ putStrLn $ Lib1.renderDataFrameAsTable s df
   where
-    terminalWidth :: (Integral n) => Maybe (Window n) -> n
+    terminalWidth :: Integral n => Maybe (Window n) -> n
     terminalWidth = maybe 80 width
-    cmd' :: Integer -> IO (Either String String)
-    cmd' s = do
-      df <- (Right <$> postWith defaults "http://localhost:1395" (Lib4.fromStatement (Lib4.SqlStatement {Lib4.statement = inp })))  --`E.catch` handleHttpException
-      case df of 
-        Left err -> return $ Left err
-        Right maybeTable -> do 
-          let table = Lib4.toTable $ maybeTable ^. Network.Wreq.responseBody :: Maybe Lib4.SqlTableFromYaml
-          let isTable = Lib4.toDataframe table 
-          case isTable of
-            Just tableIs -> return $ Lib1.renderDataFrameAsTable s isTable
-            Nothing -> return $ Left "The response contained of a bad table, we are extremely sorry"
 
+
+
+sendQuery :: String -> IO (Either ErrorMessage DataFrame)
+sendQuery query = do
+    let yamlPayload = encode query
+    let headersList = [("Content-Type", "application/x-yaml")]
+    httpResponse <- postWith (defaults & Network.Wreq.headers .~ headersList) "http://localhost:3000/query" yamlPayload
+    let decodedResponse = case decodeEither (httpResponse ^. responseBody) of
+                            Left decodeErr -> Left $ "Error decoding YAML: " ++ prettyPrintParseException decodeErr
+                            Right r -> Right r
+    case decodedResponse of
+        Left errMsg -> return $ Left errMsg
+        Right serverResp ->
+            case (responseRight serverResp, responseLeft serverResp) of
+                (Just df, _) -> return $ Right df
+                (_, Just errMsg) -> return $ Left errMsg
+                _ -> return $ Left "Invalid server response"
+            
 main :: IO ()
 main =
   evalRepl (const $ pure ">>> ") cmd [] Nothing Nothing (Word completer) ini final
